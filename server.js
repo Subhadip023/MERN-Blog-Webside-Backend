34bbf23ce85352b4caa28e94502623542d93d16e
Subhadip023 <subhadip240420@gmail.com>
2024-03-27 17:46:58 +0530

    Change it for test in local directory

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ const app = express();
 // Middleware
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: "https://mern-blog-webside.onrender.com" }));
+// app.use(cors({ credentials: true, origin: "https://mern-blog-webside.onrender.com" }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));// Change it for test in local directory 
 
 app.use(upload());
 const __filename = fileURLToPath(import.meta.url);
