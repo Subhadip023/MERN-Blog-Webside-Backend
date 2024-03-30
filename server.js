8e4f23f3cb8990c5119f8efef25cd5b9dd516b16
Subhadip023 <subhadip240420@gmail.com>
2024-03-30 09:21:50 +0530

    chage origin 6

--- server.js
+++ server.js
@@ -21,20 +21,20 @@ const app = express();
 app.use(express.json({ extended: true }));
 
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors({ credentials: true, origin: "https://mern-blog-webside.onrender.com" }));
-const allowedOrigins = [process.env.origin1, process.env.origin2];
+app.use(cors({ credentials: true, origin: "https://mern-blog-webside.onrender.com" }));
+// const allowedOrigins = [process.env.origin1, process.env.origin2];
 
-app.use(cors({
-  credentials: true,
-  origin: function (origin, callback) {
-    // Check if the origin is in the allowed origins list or if it's undefined (for requests without origin header)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+// app.use(cors({
+//   credentials: true,
+//   origin: function (origin, callback) {
+//     // Check if the origin is in the allowed origins list or if it's undefined (for requests without origin header)
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// }));
 app.use(upload());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -56,7 +56,7 @@ connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT || 5000, () => {
       console.log(`Server running on  http://localhost:${process.env.PORT || 5000}`);
-      console.log('Connected to MongoDB');
+  console.log('Connected to MongoDB');
     });
   })
   .catch(error => {
